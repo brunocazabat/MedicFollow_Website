e7a5921708d96eaf576c6910423cb3a6775f42dd
bruno cazabat <bruno.cazabat@epitech.eu>
2022-09-25 15:53:18 +0200

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,13 @@
+<script>
+// import MedicFollow from 'medic-follow'
+</script>
+
+<template>
+  <section>
+    <div>
+        <p>Hello World :)</p>
+        <p>Our Project under developement: MedicFollow</p>
+        <p>https://medicfollow.fr</p>
+    </div>
+  </section>
+</template>
